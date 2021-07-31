e6e1b1fb4b4f1272062c4da894438618de7bcd75
unknown <glodybulambo@gmail.com>
2021-08-01 00:07:42 +0200

    Adding form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState } from "react"
 import Header from "./Component/Header";
+import Tasks from "./Component/Tasks";
+import AddTask from "./Component/AddTask";
+
 function App() {
-  const name="Jantar";
+  const [tasks, setTasks]=useState([ 
+    {
+       id: 1,
+       text: 'Doctor Appointment',
+       day: 'Feb 05th at 10:30AM',
+       reminder: true,
+   },
+   {
+       id: 2,
+       text: 'Meeting at School',
+       day: 'Feb 05th at 01:00PM',
+       reminder: true,
+   },
+   {
+       id: 3,
+       text: 'Food shopping',
+       day: 'Feb 05th at 04:30PM',
+       reminder: false,
+   }])
+//Delete a task
+
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((task) => task.id!==id)
+    )}
+//Toggle Remainder
+const toggleReminder=(id)=>{
+  setTasks(tasks.map((task) => task.id===id ? {...task,reminder: !task.reminder}: task)
+  )}
+
   return (
     <div className="Container">
-      <Header/>
+      <Header />
+      <AddTask />
+      {tasks.length > 0 ?(<Tasks tasks={tasks} onDelete={deleteTask}
+       onToggle={toggleReminder} />): ("No Task to show")}
     </div>
   );
 }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-
+const onClick=()=>{
+    console.log("Button clicked")
+}
 const Header = ({title}) => {
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button color="green" text="Hello" onClick={onClick}/>
         </header>
     )
 }
