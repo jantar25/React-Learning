e12adf0d4d67ff5bc88850257a1d387f959e8754
unknown <glodybulambo@gmail.com>
2021-08-01 02:43:45 +0200

    Front End done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./Component/Tasks";
 import AddTask from "./Component/AddTask";
 
 function App() {
+  const [showAddTask,setShowAddTask]=useState(false)
   const [tasks, setTasks]=useState([ 
     {
        id: 1,
@@ -23,8 +24,14 @@ function App() {
        day: 'Feb 05th at 04:30PM',
        reminder: false,
    }])
-//Delete a task
 
+//Add a Task
+const addTask=(task)=>{
+ const id=Math.floor(Math.random()*1000)+1
+ const newTask={id,...task}
+ setTasks([...tasks,newTask])
+  }
+//Delete a task
   const deleteTask=(id)=>{
     setTasks(tasks.filter((task) => task.id!==id)
     )}
@@ -35,8 +42,8 @@ const toggleReminder=(id)=>{
 
   return (
     <div className="Container">
-      <Header />
-      <AddTask />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ?(<Tasks tasks={tasks} onDelete={deleteTask}
        onToggle={toggleReminder} />): ("No Task to show")}
     </div>
